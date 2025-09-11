643fd6e322801d6ab912c5d20b860e3d197b267e
reagancheatham <reagan.cheatham@eagles.oc.edu>
2025-09-11 17:50:27 -0500

    Working on buttons

--- JavaScript/roadmap.js
+++ JavaScript/roadmap.js
@@ -1,120 +1,18 @@
-drawSmallLines();
-drawBigLine();
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
 
-function drawSmallLines() {
-    const canvases = document.getElementsByName("roadmapCanvas");
-    const lineDefinitions = [
-        {
-            startX: -10,
-            startY: 80,
-            controlX: 0,
-            controlY: 50,
-            endX: 200,
-            endY: 60,
-            hasArrow: true,
-            arrowX: 170.5,
-            arrowY: 58.8,
-        },
-        {
-            startX: -10,
-            startY: 80,
-            controlX: 0,
-            controlY: 120,
-            endX: 200,
-            endY: 80,
-            hasArrow: true,
-            arrowX: 170.5,
-            arrowY: 86,
-        },
-        {
-            startX: -10,
-            startY: 80,
-            controlX: 70,
-            controlY: 60,
-            endX: 60,
-            endY: 180,
-            hasArrow: false,
-        },
-        {
-            startX: -10,
-            startY: 80,
-            controlX: 0,
-            controlY: 50,
-            endX: 200,
-            endY: 60,
-            hasArrow: true,
-            arrowX: 170.5,
-            arrowY: 58.8,
-        }
-    ];
+formatCards();
 
-    for (let i = 0; i < canvases.length; i++) {
-        let canvas = canvases[i];
-        let definition = lineDefinitions[i];
+function formatCards()
+{
+    leftArrow.style.display = "none";
+    const card1 = document.getElementById("element1");
+    const card2 = document.getElementById("element2");
+    const card3 = document.getElementById("element3");
 
-        const ctx = canvas.getContext("2d");
-
-        unblurLines(ctx, canvas);
-        ctx.strokeStyle = "white";
-        ctx.lineWidth = 4;
-
-        ctx.beginPath();
-        ctx.moveTo(definition.startX, definition.startY);
-        ctx.quadraticCurveTo(
-            definition.controlX,
-            definition.controlY,
-            definition.endX,
-            definition.endY
-        );
-        ctx.stroke();
-
-        if (definition.hasArrow)
-            drawArrow(ctx, definition.arrowX, definition.arrowY, 20, 0);
-    }
+    card1.style.left = "25%";
+    card2.style.left = "50%";
+    card3.style.left = "75%";
 }
 
-function drawBigLine() {
-    const longCanvas = document.getElementById("longArrowCanvas");
-    const ctx = longCanvas.getContext("2d");
-
-    unblurLines(ctx, longCanvas);
-    ctx.strokeStyle = "white";
-    ctx.lineWidth = 4;
-
-    ctx.beginPath();
-    ctx.moveTo(0, 80);
-    ctx.quadraticCurveTo(
-      50, 50, 100, 50
-    );
-    ctx.stroke();
-}
-
-function unblurLines(ctx, canvas) {
-    const dpr = window.devicePixelRatio;
-    const rect = canvas.getBoundingClientRect();
-
-    canvas.width = rect.width * dpr;
-    canvas.height = rect.height * dpr;
-
-    ctx.scale(dpr, dpr);
-
-    canvas.style.width = `${rect.width}px`;
-    canvas.style.height = `${rect.height}px`;
-}
-
-function drawArrow(ctx, posX, posY, length, angle) {
-    ctx.beginPath();
-    ctx.moveTo(posX, posY);
-    ctx.lineTo(
-        posX - length * Math.cos(angle - Math.PI / 6),
-        posY - length * Math.sin(angle - Math.PI / 6)
-    );
-    ctx.lineTo(
-        posX - length * Math.cos(angle + Math.PI / 6),
-        posY - length * Math.sin(angle + Math.PI / 6)
-    );
-    ctx.closePath();
-
-    ctx.fillStyle = "white";
-    ctx.fill();
-}
+// use requestAnimationFrame
