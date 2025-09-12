54b8d0a5d937a9c4cc33f9e5402f03b669e55bd0
reagancheatham <reagan.cheatham@eagles.oc.edu>
2025-09-12 02:46:25 -0500

    Add awesome roadmap

--- JavaScript/roadmap.js
+++ JavaScript/roadmap.js
@@ -5,37 +5,64 @@ const card2 = document.getElementById("element2");
 const card3 = document.getElementById("element3");
 const card4 = document.getElementById("element4");
 const card5 = document.getElementById("element5");
+const card6 = document.getElementById("element6");
 
-const previewClass = "roadmapPreview";
 const scrollSpeed = 0.008;
-
+const minCardBrightness = 0.5;
+const maxCardBrightness = 1.0;
 const activeCardSpacings = [25, 50, 75];
+
+const cardTarget = "roadmapElementTarget";
+const cardVisited = "roadmapElementVisited";
+const targetCardKey = "targetCard";
+
+let cards = [card1, card2, card3, card4, card5, card6];
 let activeCards = [card1, card2, card3];
-let inactiveCards = [card4, card5];
+let inactiveCards = [card4, card5, card6];
 let isScrolling = false;
 
 rightArrow.onclick = onRightArrowClicked;
+leftArrow.onclick = onLeftArrowClicked;
 
 formatCards();
 
 function formatCards() {
-    leftArrow.style.display = "none";
+    let targetCard;
 
-    card4.classList.add(previewClass);
-    card5.classList.add(previewClass);
+    if (localStorage[targetCardKey] == null) targetCard = card1;
+    else targetCard = document.getElementById(localStorage[targetCardKey]);
+
+    leftArrow.style.display = "none";
 
     card1.style.left = `${activeCardSpacings[0]}%`;
     card2.style.left = `${activeCardSpacings[1]}%`;
     card3.style.left = `${activeCardSpacings[2]}%`;
     card4.style.left = `${activeCardSpacings[0] + 75}%`;
     card5.style.left = `${activeCardSpacings[1] + 75}%`;
+    card6.style.left = `${activeCardSpacings[2] + 75}%`;
+
+    targetCard.classList.add(cardTarget);
+    setCardBrightness(card4, minCardBrightness);
+    setCardBrightness(card5, minCardBrightness);
+
+    for (let i = 0; i < cards.indexOf(targetCard); i++)
+        cards[i].classList.add(cardVisited);
+
+    if (cards.indexOf(targetCard) > 2) onRightArrowClicked();
 }
 
 function onRightArrowClicked() {
-    if (isScrolling)
-        return;
+    if (isScrolling) return;
 
     moveCardsLeft(0);
+    rightArrow.style.display = "none";
+}
+
+function onLeftArrowClicked() {
+    if (isScrolling) return;
+
+    moveCardsRight(0);
+    leftArrow.style.display = "none";
 }
 
 function moveCardsLeft(t) {
@@ -46,25 +73,86 @@ function moveCardsLeft(t) {
         const card = activeCards[i];
         const start = activeCardSpacings[i];
         const end = start - 75.0;
+        const easedT = easeInOutQuad(t);
 
-        const current = lerp(start, end, easeInOutQuad(t));
-        card.style.left = `${current}%`;
+        const pos = lerp(start, end, easedT);
+        const brightness = lerp(maxCardBrightness, minCardBrightness, easedT);
+
+        card.style.left = `${pos}%`;
+        setCardBrightness(card, brightness);
     }
 
     for (let i = 0; i < inactiveCards.length; i++) {
         const card = inactiveCards[i];
         const start = activeCardSpacings[i] + 75.0;
         const end = start - 75.0;
+        const easedT = easeInOutQuad(t);
 
-        const current = lerp(start, end, easeInOutQuad(t));
-        card.style.left = `${current}%`;
+        const pos = lerp(start, end, easedT);
+        const brightness = lerp(minCardBrightness, maxCardBrightness, easedT);
+
+        card.style.left = `${pos}%`;
+        setCardBrightness(card, brightness);
     }
 
     if (t < 1.0) requestAnimationFrame(() => moveCardsLeft(t));
     else {
         swapActiveCards();
         isScrolling = false;
+
+        leftArrow.style.display = "flex";
     }
+}
+
+function moveCardsRight(t) {
+    isScrolling = true;
+    t += scrollSpeed;
+
+    for (let i = activeCards.length - 1; i >= 0; i--) {
+        const card = activeCards[i];
+        const start = activeCardSpacings[i];
+        const end = start + 75.0;
+        const easedT = easeInOutQuad(t);
+
+        const pos = lerp(start, end, easedT);
+        const brightness = lerp(maxCardBrightness, minCardBrightness, easedT);
+
+        card.style.left = `${pos}%`;
+        setCardBrightness(card, brightness);
+    }
+
+    for (let i = 0; i < inactiveCards.length; i++) {
+        const card = inactiveCards[i];
+        const start = activeCardSpacings[i] - 75.0;
+        const end = start + 75.0;
+        const easedT = easeInOutQuad(t);
+
+        const pos = lerp(start, end, easedT);
+        const brightness = lerp(minCardBrightness, maxCardBrightness, easedT);
+
+        card.style.left = `${pos}%`;
+        setCardBrightness(card, brightness);
+    }
+
+    if (t < 1.0) requestAnimationFrame(() => moveCardsRight(t));
+    else {
+        swapActiveCards();
+        isScrolling = false;
+
+        rightArrow.style.display = "flex";
+    }
+}
+
+function setCardBrightness(card, brightness) {
+    if (brightness == 1.0)
+    {
+        card.style.filter = null;
+        return;
+    }
+
+    brightness *= 100;
+
+    card.style.filter = `brightness(${brightness}%)`;
 }
 
 function swapActiveCards() {
@@ -79,10 +167,6 @@ function swapActiveCards() {
     activeCards.length = 0;
 
     for (let card of swapCards) activeCards.push(card);
-
-    for (let card of inactiveCards) card.classList.add(previewClass);
-
-    for (let card of activeCards) card.classList.remove(previewClass);
 }
 
 function lerp(start, end, t) {
@@ -95,4 +179,10 @@ function easeInOutQuad(t) {
     if (t < 1) return 0.5 * t * t;
 
     return -0.5 * (--t * (t - 2) - 1);
+}
+
+function onCardClicked(path, elementId) {
+    localStorage[targetCardKey] = elementId;
+
+    window.location.href = path;
 }
